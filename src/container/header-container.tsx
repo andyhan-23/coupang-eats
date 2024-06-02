ac00e59ae9039adf14e195b8a3cbfaf25d580d0a
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2024-06-03 04:28:02 +0900

    feat: 헤더 컨테이너 불필요한 코드 제거

--- src/container/header-container.tsx
+++ src/container/header-container.tsx
@@ -1,15 +1,7 @@
 import Header from "@/components/header";
-import useFetchVendor from "@/hooks/use-fetch-vendor";
 
 const HeaderContainer = (props): JSX.Element => {
   const { data } = props;
-  // const { data, isLoading, error } = useFetchVendor();
-
-  // if (isLoading) return <div>로딩중 입니다</div>;
-  // if (error) {
-  //   if (import.meta.env.DEV)
-  //     console.log("가게 데이터를 받아오는 도중 에러 발생:", error);
-  // }
 
   return <Header name={data.name} />;
 };
