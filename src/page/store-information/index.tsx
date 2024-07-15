45e167d847d0c6ca90b6941e63abe49e5e2070da
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2024-07-15 17:35:50 +0900

    chore: 디자인을 chakra ui에서 tailwind css로 변경

--- src/page/store-information/index.tsx
+++ src/page/store-information/index.tsx
@@ -1,48 +1,40 @@
-import { UI, AppUi } from "@/layout";
-import { Box, Text, Flex, VStack } from "@chakra-ui/react";
 import useFetchVendor from "@/hooks/use-fetch-vendor";
-import storeMap from "@/images/storeMap.jpeg";
 
 const StoreInformationPage = () => {
   const { data: vendorData, isLoading: vendorDataLoading } = useFetchVendor();
 
-  if (vendorDataLoading) return <div>가게 데이터 로딩중입니다.</div>;
+  if (vendorDataLoading)
+    return <div className="p-4">가게 데이터 로딩중입니다.</div>;
 
   return (
-    <UI>
-      <AppUi>
-        <Flex justify="center" align="center">
-          <Text fontSize="4xl" fontWeight="bold" textAlign="center">
-            매장정보
-          </Text>
-        </Flex>
-        <VStack alignItems="flex-start" spacing={4}>
-          <Text fontWeight="bold" fontSize="2xl">
-            {vendorData.name}
-          </Text>
-          <Text fontSize="1xl">{vendorData.address}</Text>
-          <Text>
-            전화번호{" "}
-            <Text as="span" color="skyblue">
-              {vendorData.phoneNumber}
-            </Text>
-          </Text>
-          <Text>대표자명: {vendorData.representativeName}</Text>
-          <Text>사업자등록번호: {vendorData.companyResgistrationNumber}</Text>
-          <Text>상호명: {vendorData.companyName}</Text>
-
-          <Text fontWeight="bold" fontSize="2xl">
-            영업시간
-          </Text>
-          <Text>{vendorData.operationHour}</Text>
-
-          <Text fontWeight="bold" fontSize="2xl">
-            매장소개
-          </Text>
-          <Text>{vendorData.storeIntroduction}</Text>
-        </VStack>
-      </AppUi>
-    </UI>
+    <div className="bg-white">
+      <div className="max-w-4xl mx-auto p-8">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold">매장정보</h1>
+        </div>
+        <div className="mt-8 space-y-4">
+          <div>
+            <h2 className="text-2xl font-bold">{vendorData.name}</h2>
+            <p className="text-xl">{vendorData.address}</p>
+            <p className="mt-2">
+              전화번호{" "}
+              <span className="text-skyblue">{vendorData.phoneNumber}</span>
+            </p>
+            <p>대표자명: {vendorData.representativeName}</p>
+            <p>사업자등록번호: {vendorData.companyResgistrationNumber}</p>
+            <p>상호명: {vendorData.companyName}</p>
+          </div>
+          <div>
+            <h2 className="text-2xl font-bold">영업시간</h2>
+            <p>{vendorData.operationHour}</p>
+          </div>
+          <div>
+            <h2 className="text-2xl font-bold">매장소개</h2>
+            <p>{vendorData.storeIntroduction}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
